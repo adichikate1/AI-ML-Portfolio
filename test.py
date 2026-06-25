b4bc6fd5ec5396d9010bafee86f6180b56d553cb
adichikate1 <adichikate@gmail.com>
2026-06-25 17:28:55 +0530

    Update portfolio with new files

--- test.py
+++
@@ -1,8 +0,0 @@
-from sklearn.tree import DecisionTreeClassifier
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-
-df = pd.read_csv("static/data/tested.csv")
-df["Title"] = df["Name"].str.extract(r",\s*([^\.]+)\.")
-print(df["Title"].unique())
